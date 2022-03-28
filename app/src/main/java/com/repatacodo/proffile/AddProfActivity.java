d71ff8f92f3c229739b28705cd7616f48d169d68
Miguel <miguelandrerepatacodo@gmail.com>
2022-03-28 15:04:36 +0800

    Approaching MVP#1

---
+++ app/src/main/java/com/repatacodo/proffile/AddProfActivity.java
@@ -0,0 +1,14 @@
+package com.repatacodo.proffile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_prof);
+    }
+}
--- app/src/main/java/com/repatacodo/proffile/MainActivity.java
+++ app/src/main/java/com/repatacodo/proffile/MainActivity.java
@@ -2,13 +2,45 @@ package com.repatacodo.proffile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button RecyclerViewPlaceholder;
+    Button FloatingActionButtonPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+
     }
+
+    private void initializeViews(){
+        //set up the recycler views for the profs
+        RecyclerViewPlaceholder = findViewById(R.id.button);
+        RecyclerViewPlaceholder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //set up the floating action button to add more profs
+        FloatingActionButtonPlaceholder = findViewById(R.id.button2);
+        FloatingActionButtonPlaceholder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddProfActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
 }
